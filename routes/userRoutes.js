5a134bbd0075e5aa16d794b2bf8d2edeb50998f1
Meek Meek <haggins.jameka@gmail.com>
2026-03-29 21:01:25 -0400

    created the userRoutes.js which is handling registering, loggin in, and kicking off the github flow.

---
+++ routes/userRoutes.js
@@ -0,0 +1,54 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import passport from 'passport';
+import User from "../models/User.js";
+import { generateToken } from '../utils/auth.js';
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({ email, password: hashedPassword });
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !user.password) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: "Invalid Credentials" });
+
+    const token = generateToken(user);
+    res.join({ token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get('/auth/github/callback',
+  passport.authenticate('github', { session: false }),
+  (req, res) => {
+    const token = generateToken(req.user);
+
+    res.redirect(`http://localhost:3001/api/bookmarks?token=${token}`);
+  }
+);
+
+export default router;
